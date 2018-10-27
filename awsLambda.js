eb8b3bad374f499a78df5fef56848815d48ee7ac
Martti Laine <martti_stock@indoles.com>
2018-10-27 14:38:41 -0400

    added aws lambda file

---
+++ awsLambda.js
@@ -0,0 +1,36 @@
+/* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
+/**
+ * This sample demonstrates a simple skill built with the Amazon Alexa Skills
+ * nodejs skill development kit.
+ * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
+ * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
+ * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
+ **/
+
+'use strict';
+const Alexa = require('alexa-sdk');
+
+const handlers = {
+    'LaunchRequest': function () {
+        
+    },
+    'GetNewFactIntent': function () {
+       
+    },
+    'AMAZON.HelpIntent': function () {
+        
+    },
+    'AMAZON.CancelIntent': function () {
+        
+    },
+    'AMAZON.StopIntent': function () {
+    
+    },
+};
+
+exports.handler = function (event, context, callback) {
+    const alexa = Alexa.handler(event, context, callback);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
